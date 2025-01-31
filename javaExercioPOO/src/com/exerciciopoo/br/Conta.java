3e2907735acd89a3aa9084745cdd54d11a9520e2
thiagonatalicio <thiagonatalicio33@gmail.com>
2025-01-31 16:19:10 -0300

    criar conta
    função de criar conta

--- javaExercioPOO/src/com/exerciciopoo/br/Conta.java
+++ javaExercioPOO/src/com/exerciciopoo/br/Conta.java
@@ -1,5 +1,5 @@
 package com.exerciciopoo.br;
-
+import java.util.Scanner;
 public class Conta {
     private String propietario;
     private int numero;
@@ -10,6 +10,46 @@ public class Conta {
 
     public Conta(){
         this.saldo = 0;
+    }
+
+    class Builder{
+        private String propietario;
+        private int numero;
+        private int cpfPropietario;
+        private String tipo;
+        private double saldo;
+        private int senha;
+    }
+    public Builder cadastrarConta(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\nCriando nova conta.");
+        System.out.println("Digite seu nome:");
+        System.out.print("=[>");
+        String nome = scanner.nextLine();
+        System.out.println("\nDigite seu CPF:");
+        System.out.print("=[>");
+        int cpfPropietario = scanner.nextInt();
+        System.out.println("\nDigite 1 para conta poupança e 2 para corrente:");
+        System.out.print("=[>");
+        String tipo;
+        int escolha = 0;
+        do{
+            escolha = scanner.nextInt();
+            if(escolha == 1){
+                tipo = "Poupança";
+            }
+            if(escolha == 2){
+                tipo = "Corrente";
+            }
+        }while(escolha != 1 && escolha != 2);
+        int senha;
+        do{
+            System.out.println("\nCrie uma senha de no maximo 6 digitos:");
+            System.out.print("=[>");
+            senha = scanner.nextInt();
+        }while(senha > 0 && senha < 999999);
+        Builder conta = new Builder();
+        return conta;
     }
 
     public void sacar(double valor){
--- javaExercioPOO/src/com/exerciciopoo/br/Main.java
+++ javaExercioPOO/src/com/exerciciopoo/br/Main.java
@@ -4,15 +4,7 @@ package com.exerciciopoo.br;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Conta conta = new Conta();
-        conta.extrato();
-        conta.deposito(5.78);
-        conta.extrato();
-        conta.sacar(10);
-        conta.extrato();
-        conta.deposito(3.45);
-        conta.extrato();
-        conta.sacar(7);
-        conta.extrato();
+        Conta conta1 = new Conta();
+
     }
 }
